d2ef7fa7061ecc7d27c16b94c11ffb1bea6b9745
Henning Sato von Rosen <henning.von.rosen@gmail.com>
2021-12-01 10:24:49 +0100

    yeeah

--- apps/app01--BasicCounter.js
+++ apps/app01--BasicCounter.js
@@ -1,28 +1,44 @@
 const num2sign = n => [ 'negative', 'zero', 'positive' ][ Math.sign( n ) + 1 ];
 
 
-[ 'def BasicCounter'
-, [ '::<int>' ]
-, [ '<h1> title', "Basic Counter" ]
-, [ '<div> display' ]
-, [ '<div> buttons'
-  , [ '<button>', " -10", [ 'on click',   n => [ n-10  ] ] ]         // DECREMENT
-  , [ '<button>', "  -1", [ 'on click',   n => [ n-1   ] ] ]         // DECREMENT
-  , [ '<button>', "   0", [ 'on click',  _n => [ 0     ] ] ]         // RESET
-  , [ '<button>', "  +1", [ 'on click',   n => [ n+1   ] ] ]         // INCREMENT
-  , [ '<button>', " +10", [ 'on click',   n => [ n+10  ] ] ]         // INCREMENT
-  ]
-// Vertical
-, [ '->INIT', ()=>[0] ]
-, [ '^display-stuff', n => 
-    [ '--'
-    , [ 'the display'
-      , n                              // display number
-      , [ $$1.class, num2sign( n ) ]   // visualize sign
-      ]
+  [ 'def BasicCounter' 
+  , [ '::<int>' ] // the value of this layer is an integer
+  , [ '<h1> title', "Basic Counter" ]
+  , [ '<div> display' // embodiments are inlined using lexical scope
+    , n => n.toString()
+    , n => [ 'class', num2sign( n ) ]
     ]
-  ]
-].read().mount(911);
+  , [ '<div> buttons' // transition defs are inlined
+    , [ '<button>', "  -1", [ '!click',   n => State( n-1  ) ] ] 
+    , [ '<button>', "   0", [ '!click',  _n => State( 0    ) ] ] 
+    , [ '<button>', "  +1", [ '!click',   n => State( n+1  ) ] ]
+    ]
+  , [ 'INIT', () => State( 0 ) ] // Initial transition
+  ].read(); // 11sLOC
+
+  [ 'def BasicCounter' 
+  // Layer type definition
+  , [ '::<int>' ] // the value of this layer is an integer
+  // Body tags
+  , [ '<h1> title', "Basic Counter" ]
+  , [ '<div> display' ]
+  , [ '<div> buttons' 
+  //  Buttons with event-to-transition bindings
+    , [ '<button>', "  -1", [ '!click', 'DECR'  ] ]
+    , [ '<button>', "   0", [ '!click', 'RESET' ] ] 
+    , [ '<button>', "  +1", [ '!click', 'INCR'  ] ]
+    ]
+    // Transition definitions
+  , [ 'INIT',  () => State( 0 ) ] // Initial transition
+  , [ 'DECR',   n => State( n-1  ) ] 
+  , [ 'RES' ,  _n => State( 0    ) ] 
+  , [ 'INCR',   n => State( n+1  ) ]
+    // Embodiments
+  , n => [ 'the display', n ]
+  , n => [ 'the display', [ 'class', num2sign( n ) ] ]
+  ].read(); // 14sLOC
+
+
 
 
 
@@ -38,6 +54,6 @@ const num2sign = n => [ 'negative', 'zero', 'positive' ][ Math.sign( n ) + 1 ];
   ]
 // Vertical
 , [ '->INIT', 0 ]
-, [ '^show-number', n => [ 'the display', n ] ]                         // display number
+, [ '     ^show-number', n => [ 'the display', n ] ]                    // display number
 , [ '^show-sign'  , n => [ 'the display', [ $$1.class, num2sign( n ) ] ] ]  // visualize sign 
 ].read();
